62dcbcb7039bc216d661606f707727f1e167c796
unknown <mobilepig@naver.com>
2024-01-28 12:10:45 +0900

    ok

--- sparoutertest/src/App.vue
+++ sparoutertest/src/App.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="container">
-    <h1>hello app vue</h1>
+    <h5>hello app vue</h5>
     <router-view></router-view>
-    <router-link to="/">main</router-link>
-    <router-link to="/muzzi">muzzi</router-link>
-    <router-link to="/dori">dori</router-link>
+
+    <button class="btn btn-primary" v-on:click="$router.push('/')">
+      main 컴포넌트 가즈아
+    </button>
+    <button class="btn btn-primary" v-on:click="$router.push('muzzi')">
+      muzzi 컴포넌트 가즈아
+    </button>
+    <button class="btn btn-primary" v-on:click="$router.push('dori')">
+      dori 컴포넌트 가즈아
+    </button>
+    <button class="btn btn-danger" v-on:click="redirectToHiUrl">
+      personal 컴포넌트 가즈아
+    </button>
   </div>
 </template>
 
@@ -18,6 +28,12 @@ export default {
     return {
       data,
     };
+  },
+  methods: {
+    redirectToHiUrl() {
+      // /hiurl로 이동
+      window.location.href = "/hiurl";
+    },
   },
 };
 </script>
